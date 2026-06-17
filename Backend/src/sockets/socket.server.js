2500a7fbffd357a0e9c12c70cfcac61e5599d6aa
ANANYA160805 <ananyasinha1599@outlook.com>
2026-06-17 12:58:44 +0530

    socket

--- Backend/src/sockets/socket.server.js
+++ Backend/src/sockets/socket.server.js
@@ -2,6 +2,7 @@ const { Server } = require('socket.io');
 const cookie = require('cookie');
 const jwt = require('jsonwebtoken');
 const userModel = require('../models/user.model');
+const aiService = require('../services/ai.service');
 
 function initSocketServer(httpServer) {
     const io = new Server(httpServer, {});
@@ -28,8 +29,26 @@ function initSocketServer(httpServer) {
     });
 
     io.on('connection', (socket) => {
-        console.log('A user connected:', socket.user);
-        console.log('A user connected:', socket.id);
+
+        socket.on("ai-message", async (messagePayload) => {
+            console.log("Received  message from client:", messagePayload);
+/* 
+messagePayload should contain:
+{
+    content: "User's message to AI",
+    chat: "Chat ID or context"
+}
+*/
+            const response = await aiService.generateResponse(messagePayload.content);
+
+            socket.emit("ai-response", {
+                 content: response,
+                 chat: messagePayload.chat
+                 
+
+             });
+        });
+
     });
 
     
